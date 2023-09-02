e9f5eaf964ebef61c269a0e06d36ee24ae41af08
Luiz Tanure <letanure@gmail.com>
2023-09-02 09:51:07 +0200

    feat: Add place-category controller, router, and service

---
+++ src/api/place-category/controllers/place-category.ts
@@ -0,0 +1,7 @@
+/**
+ * place-category controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::place-category.place-category');
---
+++ src/api/place-category/routes/place-category.ts
@@ -0,0 +1,7 @@
+/**
+ * place-category router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::place-category.place-category');
