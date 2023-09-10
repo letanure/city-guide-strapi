41e12fc19b3cb9360fd83e163f9393c58d3102a9
Luiz Tanure <letanure@gmail.com>
2023-09-10 22:52:14 +0200

    refactor: simplify config files

--- config/admin.ts
+++ config/admin.ts
@@ -1,13 +1,16 @@
-export default ({ env }) => ({
+interface Env {
+  env: (key: string, defaultValue?: string) => string;
+}
+export default ({ env }: Env) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT"),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env("TRANSFER_TOKEN_SALT"),
     },
   },
 });
